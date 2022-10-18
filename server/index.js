6338b3a0de6c1cfe39c7bc411a49769cab468cda
Luke Passey <ljpassey@gmail.com>
2022-10-18 13:01:03 -0600

    This commit includes the completed demo walkthrough with the end result being a functioning movie list that you can add, rate, and remove movies.

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+//------------------//
+
+const { getMovies, deleteMovie, createMovie } = require('./controller.js')
+
+//----endpoints-----//
+
+app.get('/api/movies', getMovies)
+app.delete('/api/movies/:id', deleteMovie)
+app.post('/api/movies', createMovie)
+
+//-----listen-------//
+
+app.listen(4004, () => {
+    console.log('Up on 4004!')
+})
+
